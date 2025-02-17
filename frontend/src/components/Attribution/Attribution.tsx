9f9c85633f50f5cc0dbf74470a1726ab284d6ceb
codeinpieces <68757620+codeinpieces@users.noreply.github.com>
2025-02-16 22:11:34 -0500

    Add extra requirements to the frontend

--- frontend/src/components/Attribution/Attribution.tsx
+++ frontend/src/components/Attribution/Attribution.tsx
@@ -2,7 +2,8 @@
 
 const Attribution = () => {
     return (
-        <div>
+        <div className='text-center text-white text-sm/6 mt-10'>
+            <h2>Attributions</h2>
               <a href="https://www.flaticon.com/free-icons/metaverse" title="metaverse icons">
               Metaverse icons created by Victoruler - Flaticon
               </a>
